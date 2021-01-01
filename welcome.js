fe48bab8bb4ea01ad095b18a9a04c5c2f218c300
Christian Thompson <cjthomp2005@outlook.com>
2021-01-01 17:31:17 -0600

    i really just fucking want this to work

--- welcome.js
+++ welcome.js
@@ -3,28 +3,35 @@ const { cache } = require('./commands/setWelcome')
 const welcomeSchema = require("./schemas/welcome-schema")
 
 module.exports = (client) => {
-    const onJoin = async member => {
+    const onJoin = async (member) => {
         const { guild } = member
+
         let data = cache[guild.id]
-        if(!data) {
-            console.log("FETCHING FROM DATABASE")
-            await mongo(),then( async mongoose => {
+
+        if (!data) {
+            console.log('FETCHING FROM DATABASE')
+
+            await mongo().then(async (mongoose) => {
                 try {
                     const result = await welcomeSchema.findOne({ _id: guild.id })
-                    cache[guild.id] = data = [result.channel.id, result.text]
+
+                    cache[guild.id] = data = [result.channelId, result.text]
                 } finally {
                     mongoose.connection.close()
                 }
-            })
+             })
         }
 
         const channelId = data[0]
-        const text = data [1]
+        const text = data[1]
+
         const channel = guild.channels.cache.get(channelId)
         channel.send(text)
     }
-       
+    
     client.on('guildMemberAdd', member => {
+        console.log('member joined')
         onJoin(member)
-    })
+    });
+    
 }
